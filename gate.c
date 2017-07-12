db6c050249c963585c63cc76a332edfc8c07f7ed
eLKei <elkei.kr@gmail.com>
2017-07-12 16:57:56 +0900

    a step to Universe

---
+++ gate.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "universe.h"
+
+int main( void )
+{
+    openTheDoorToUniverse();
+    return 0;
+}
---
+++ universe.h
@@ -0,0 +1,13 @@
+#ifndef _UNIVERSE_H_
+#define _UNIVERSE_H_
+
+void openTheDoorToUniverse( void ){
+  printf("Wellcome to the Our Universe\n");
+}
+
+void closeTheDoorToHell( void )
+{
+  printf("Hope you back to Hell~\n");
+}
+
+#endif /* _UNIVERSE_H_ */
